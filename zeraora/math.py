c8ac9bb7191ee0663f5651c91c7bfbe6a1a9dc0e
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-03-23 18:23:38 +0800

    feat: 添加去除尾导零的函数

---
+++ zeraora/math.py
@@ -0,0 +1,10 @@
+from decimal import Decimal
+
+
+def remove_exponent(d: Decimal):
+    """
+    去除十进制小数（Decimal）的尾导零。
+
+    https://docs.python.org/zh-cn/3/library/decimal.html#decimal-faq
+    """
+    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
