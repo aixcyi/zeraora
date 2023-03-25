3a7ac621d2332fe70d5cf884637b05bf1b285e53
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-03-25 15:16:26 +0800

    docs: 更正remove_exponent的文档注释

--- zeraora/math.py
+++ zeraora/math.py
@@ -1,3 +1,7 @@
+__all__ = [
+    'remove_exponent',
+]
+
 from decimal import Decimal
 
 
@@ -5,6 +9,7 @@ def remove_exponent(d: Decimal):
     """
     去除十进制小数（Decimal）的尾导零。
 
+    非原创代码，出自：
     https://docs.python.org/zh-cn/3/library/decimal.html#decimal-faq
     """
     return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
