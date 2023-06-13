d0a83a5d2db6337fdf653f89a23f789fd5cde82b
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-06-13 11:22:16 +0800

    fix: 补充丢失的装饰器start

--- zeraora/utils.py
+++ zeraora/utils.py
@@ -356,3 +356,25 @@ class ReprMixin(object):
 
     def _obtain_kls(self) -> str:
         return self.__class__.__name__
+
+
+def start(*version: int, note=None):
+    """
+    检查 Python 版本是否高于或等于指定值，
+    如果低于指定的版本就会抛出 RuntimeError。
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if sys.version_info < version:
+                raise RuntimeError(
+                    f'Require Python version {version:s} to run. '
+                    f'运行此函数/类需要Python版本在 {version:s} 及以上。'
+                    + ('' if note is None else note)
+                )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
