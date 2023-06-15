5af1ef9bd591e6a0f84e29bdd153d0bea4ec7514
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-06-16 00:22:09 +0800

    test: 化繁为简

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -1,20 +1,9 @@
-from typing import Tuple, Union
 from unittest import TestCase
 
 
 class BaseTestCase(TestCase):
 
-    def assertHasAttribute(self, attr: str, obj):
-        self.assertTrue(hasattr(obj, attr))
-
-    def assertNoAttribute(self, attr: str, obj):
-        self.assertFalse(hasattr(obj, attr))
-
-    def assertLengthEqual(self, length: int, exp):
-        self.assertEqual(length, len(exp))
-
     def assertMemberTypeIs(self, _type: type, exp):
-        self.assertTrue(all(type(member) is _type for member in exp))
-
-    def assertMemberIsInstance(self, _type: Union[type, Tuple[type]], exp):
-        self.assertTrue(all(isinstance(member, _type) for member in exp))
+        t0 = (_type,) * len(exp)
+        t2 = tuple(type(member) for member in exp)
+        self.assertTupleEqual(t0, t2)
