c0bd395e1096032d0ea2d309bd27e76d116eda0e
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2023-07-19 00:29:55 +0800

    refactor: 弃用用于取消click命令自动退出的装饰器

--- zeraora/click/decorators.py
+++ zeraora/click/decorators.py
@@ -1,12 +1,16 @@
 from functools import update_wrapper
 from typing import Any, Callable
 
+import zeraora
+from zeraora.utils import deprecate
+
 try:
     import click
 except ImportError as e:
     raise ImportError('需要安装click框架：\npip install click') from e
 
 
+@deprecate(0, 3, 6, ref=zeraora.__version__)
 def pass_context_without_exit(f: Callable[..., Any]) -> Callable[..., Any]:
     """
     将上下文作为被装饰的方法的第一个参数，并取消自动退出。（不能与 click.pass_context 连用）
@@ -21,6 +25,20 @@ def pass_context_without_exit(f: Callable[..., Any]) -> Callable[..., Any]:
     >>>
     >>> if __name__ == '__main__':
     >>>     calculating(['dummy'])
+    >>>     print('calculate completed.')
+
+    该方案不能应付所有情况，故被弃用。
+    使用 ``standalone_mode`` 参数可完美解决此问题：
+
+    >>> import click
+    >>>
+    >>> @click.command('ratio')
+    >>> @click.argument('argument')
+    >>> def calculating(ctx, argument):
+    >>>     pass
+    >>>
+    >>> if __name__ == '__main__':
+    >>>     calculating(['dummy'], standalone_mode=False)
     >>>     print('calculate completed.')
     """
 
