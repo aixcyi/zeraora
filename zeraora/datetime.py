3241906333d1758d8542f96b9ee4e009e819986f
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-09-03 11:03:08 +0800

    refactor(datetime): 移除 DateTime，不再重复造轮子

--- zeraora/datetime.py
+++ zeraora/datetime.py
@@ -4,149 +4,16 @@
 from __future__ import annotations
 
 __all__ = [
-    'DateTime',
-    'DateRange',
     'BearTimer',
 ]
 
 import logging
 import sys
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from functools import wraps
 from threading import Lock
-from typing import Generator
 
 logger = logging.getLogger('zeraora.datetime')
-
-
-class DateTime(datetime):
-    """
-    增强型日期时间对象。
-    """
-
-    # -------- 装箱拆箱 --------
-
-    @classmethod
-    def of(cls, dt: datetime) -> DateTime:
-        return cls(
-            dt.year, dt.month, dt.day,
-            dt.hour, dt.minute, dt.second,
-            dt.microsecond, dt.tzinfo, fold=dt.fold
-        )
-
-    def to(self) -> datetime:
-        return datetime(
-            self.year, self.month, self.day,
-            self.hour, self.minute, self.second,
-            self.microsecond, self.tzinfo, fold=self.fold
-        )
-
-    # -------- 快速变换 --------
-
-    def start_of_year(self) -> DateTime:
-        """
-        将日期部分固定为当年的1月1日。
-        """
-        return self.replace(month=1, day=1)
-
-    def end_of_year(self) -> DateTime:
-        """
-        将日期部分固定为当年的12月31日。
-        """
-        return self.replace(month=12, day=31)
-
-    def start_of_day(self) -> DateTime:
-        """
-        将时间部分固定为 ``00:00:00.000000`` 。
-        """
-        return self.replace(hour=0, minute=0, second=0, microsecond=0)
-
-    def end_of_day(self) -> DateTime:
-        """
-        将时间部分固定为 ``23:59:59.999999`` 。
-        """
-        return self.replace(hour=23, minute=59, second=59, microsecond=10 ** 6 - 1)
-
-    # -------- 计算方法 --------
-
-    @classmethod
-    def of_week(cls, year: int, week_in_year: int, day_in_week: int, sunday_first=False) -> DateTime:
-        """
-        将某一年的某一周的星期几转换为一个具体的日期。
-
-        :param year: 具体年份。比如 2012、2023 等。
-        :param week_in_year: 一年中的第几周。从 0 开始。
-        :param day_in_week: 星期几。0 表示周日、1 表示周一，以此类推。
-        :param sunday_first: 是否以周日为一周的开始。
-        :return: 一个 DateTime 对象。
-        """
-        day = f'{year:04d}-{week_in_year:02d}-{day_in_week:1d}'
-        fmt = '%Y-%U-%w' if sunday_first else '%Y-%W-%w'
-        return cls.strptime(day, fmt)
-
-    def get_week(self, sunday_first=False) -> int:
-        """
-        计算自当年开始的周序号。
-
-        一年中第一个星期一（如果 ``sunday_first`` 为 ``True`` 则是星期日）之前的日子都算作第 ``0`` 周。
-
-        :param sunday_first: 是否以周日作为一周的开始。
-        :return: 一个从 0 开始递增的整数。
-        """
-        return int(self.strftime('%U') if sunday_first else self.strftime('%W'))
-
-    get_week_in_year = get_week
-
-    def get_week_in_month(self, sunday_first=False) -> int:
-        """
-        计算自当月开始的周序号。
-
-        一个月中第一个星期一（如果 ``sunday_first`` 为 ``True`` 则是星期日）之前的日子都算作第 ``0`` 周。
-
-        :param sunday_first: 是否以周日作为一周的开始。
-        :return: 一个从 0 开始递增的整数。
-        """
-        # TODO: 待办
-
-
-class DateRange:
-    """
-    日期范围工具类。
-    """
-
-    @staticmethod
-    def walk(start: date, stop: date, step: int = 1, closed=False) -> Generator[date, None, None]:
-        """
-        根据起始日期确定一个日期范围。
-
-        :param start: 开始日期。
-        :param stop: 结束日期。
-        :param step: 步长。提供负数时，请确保开始日期晚于（即大于）结束日期。如果为 ``0`` 会引发 ``ValueError`` 。
-        :param closed: 结束日期是否可以到达。
-        :return: 迭代元素类型为 ``date`` 的可迭代对象。
-        """
-        if closed:
-            ordinals = range(start.toordinal(), stop.toordinal() + step, step)
-        else:
-            ordinals = range(start.toordinal(), stop.toordinal(), step)
-
-        return (date.fromordinal(o) for o in ordinals)
-
-    @staticmethod
-    def by_week(year: int, week_in_year: int, sunday_first=False) -> Generator[date, None, None]:
-        """
-        计算一年中某一周对应的日期范围。
-
-        :param year: 具体年份。比如 2012、2023 等。
-        :param week_in_year: 一年中的第几周。从 0 开始。
-        :param sunday_first: 是否以周日为一周的开始。
-        :return: 迭代元素类型为 ``date`` 的可迭代对象。
-        """
-        start = datetime.strptime(
-            f'{year:04d}-{week_in_year:02d}-{0 if sunday_first else 1}',
-            '%Y-%U-%w' if sunday_first else '%Y-%W-%w',
-        ).date()
-        return (start + timedelta(days=i) for i in range(7))
 
 
 class BearTimer:
