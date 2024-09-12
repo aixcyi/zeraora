b5c188e6d437274e9db22b6692d4e58cd9df1664
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-09-12 12:15:58 +0800

    feat(datetime): 新增日期生成器、Datetime、Timedelta、闰年判断、月末求值等

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,6 +1,7 @@
 import unittest
+from datetime import datetime
 
-from zeraora.datetime import BearTimer
+from zeraora.datetime import *
 
 
 class DatetimeTest(unittest.TestCase):
@@ -22,3 +23,89 @@ class DatetimeTest(unittest.TestCase):
                 return sum(range(length))
 
             _ = calc_summary(10_0000)
+
+    def test_is_leap(self):
+        self.assertFalse(is_leap(1700), '1700 is not a leap year.')
+        self.assertFalse(is_leap(1800), '1800 is not a leap year.')
+        self.assertFalse(is_leap(1970), '1970 is not a leap year.')
+        self.assertTrue(is_leap(2000), '2000 is a leap year.')
+        self.assertTrue(is_leap(2020), '2020 is a leap year.')
+        self.assertFalse(is_leap(2021), '2021 is not a leap year.')
+        self.assertFalse(is_leap(2022), '2022 is not a leap year.')
+        self.assertFalse(is_leap(2023), '2023 is not a leap year.')
+        self.assertTrue(is_leap(2024), '2024 is a leap year.')
+
+    def test_get_last_monthday(self):
+        self.assertEqual(28, get_last_monthday(1970, 2))
+        self.assertEqual(29, get_last_monthday(2020, 2))
+        self.assertEqual(28, get_last_monthday(2021, 2))
+        self.assertEqual(28, get_last_monthday(2022, 2))
+        self.assertEqual(28, get_last_monthday(2023, 2))
+        self.assertEqual(31, get_last_monthday(2024, 1))
+        self.assertEqual(29, get_last_monthday(2024, 2))
+        self.assertEqual(31, get_last_monthday(2024, 3))
+        self.assertEqual(30, get_last_monthday(2024, 4))
+        self.assertEqual(31, get_last_monthday(2024, 5))
+        self.assertEqual(30, get_last_monthday(2024, 6))
+        self.assertEqual(31, get_last_monthday(2024, 7))
+        self.assertEqual(31, get_last_monthday(2024, 8))
+        self.assertEqual(30, get_last_monthday(2024, 9))
+        self.assertEqual(31, get_last_monthday(2024, 10))
+        self.assertEqual(30, get_last_monthday(2024, 11))
+        self.assertEqual(31, get_last_monthday(2024, 12))
+
+    def testDatetime(self):
+        self.assertEqual(datetime(2011, 1, 29), Datetime.fromcalendar(2011, 4, 6))
+        self.assertEqual(datetime(2011, 1, 30), Datetime.fromcalendar(2011, 4, 0))
+        self.assertEqual(datetime(2011, 1, 31), Datetime.fromcalendar(2011, 5, 1))
+        self.assertEqual(datetime(2011, 2, 1), Datetime.fromcalendar(2011, 5, 2))
+        self.assertEqual(datetime(2011, 2, 2), Datetime.fromcalendar(2011, 5, 3))
+        self.assertEqual(datetime(2011, 2, 3), Datetime.fromcalendar(2011, 5, 4))
+        self.assertEqual(datetime(2011, 2, 4), Datetime.fromcalendar(2011, 5, 5))
+        self.assertEqual(datetime(2011, 2, 5), Datetime.fromcalendar(2011, 5, 6))
+        self.assertEqual(datetime(2011, 2, 6), Datetime.fromcalendar(2011, 5, 0))
+        self.assertEqual(datetime(2011, 2, 7), Datetime.fromcalendar(2011, 6, 1))
+        self.assertEqual(datetime(2011, 2, 8), Datetime.fromcalendar(2011, 6, 2))
+
+        self.assertEqual(datetime(2011, 1, 29), Datetime.fromcalendar(2011, 4, 6, sunday_first=True))
+        self.assertEqual(datetime(2011, 1, 30), Datetime.fromcalendar(2011, 5, 0, sunday_first=True))
+        self.assertEqual(datetime(2011, 1, 31), Datetime.fromcalendar(2011, 5, 1, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 1), Datetime.fromcalendar(2011, 5, 2, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 2), Datetime.fromcalendar(2011, 5, 3, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 3), Datetime.fromcalendar(2011, 5, 4, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 4), Datetime.fromcalendar(2011, 5, 5, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 5), Datetime.fromcalendar(2011, 5, 6, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 6), Datetime.fromcalendar(2011, 6, 0, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 7), Datetime.fromcalendar(2011, 6, 1, sunday_first=True))
+        self.assertEqual(datetime(2011, 2, 8), Datetime.fromcalendar(2011, 6, 2, sunday_first=True))
+
+        self.assertTupleEqual((2011, 4, 6), Datetime(2011, 1, 29).calendar())
+        self.assertTupleEqual((2011, 4, 0), Datetime(2011, 1, 30).calendar())
+        self.assertTupleEqual((2011, 5, 1), Datetime(2011, 1, 31).calendar())
+        self.assertTupleEqual((2011, 5, 2), Datetime(2011, 2, 1).calendar())
+        self.assertTupleEqual((2011, 5, 3), Datetime(2011, 2, 2).calendar())
+        self.assertTupleEqual((2011, 5, 4), Datetime(2011, 2, 3).calendar())
+        self.assertTupleEqual((2011, 5, 5), Datetime(2011, 2, 4).calendar())
+        self.assertTupleEqual((2011, 5, 6), Datetime(2011, 2, 5).calendar())
+        self.assertTupleEqual((2011, 5, 0), Datetime(2011, 2, 6).calendar())
+        self.assertTupleEqual((2011, 6, 1), Datetime(2011, 2, 7).calendar())
+        self.assertTupleEqual((2011, 6, 2), Datetime(2011, 2, 8).calendar())
+
+        self.assertTupleEqual((2011, 4, 6), Datetime(2011, 1, 29).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 0), Datetime(2011, 1, 30).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 1), Datetime(2011, 1, 31).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 2), Datetime(2011, 2, 1).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 3), Datetime(2011, 2, 2).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 4), Datetime(2011, 2, 3).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 5), Datetime(2011, 2, 4).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 5, 6), Datetime(2011, 2, 5).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 6, 0), Datetime(2011, 2, 6).calendar(sunday_first=True))
+        self.assertTupleEqual((2011, 6, 1), Datetime(2011, 2, 7).calendar(sunday_first=True))
+
+        today = datetime(2011, 2, 3)
+        self.assertEqual(today, Datetime.fromdatetime(today))
+        self.assertEqual(today, Datetime.of(today))
+        self.assertEqual(today, Datetime.of(today).replace(standard=True))
+        self.assertIs(Datetime, type(Datetime.fromdatetime(today)))
+        self.assertIs(Datetime, type(Datetime.of(today)))
+        self.assertIs(datetime, type(Datetime.of(today).replace(standard=True)))
--- zeraora/datetime.py
+++ zeraora/datetime.py
@@ -5,13 +5,22 @@ from __future__ import annotations
 
 __all__ = [
     'BearTimer',
+    'is_leap',
+    'get_last_monthday',
+    'TimeFrame',
+    'Datetime',
+    'Timedelta',
+    'daterange',
+    'weekrange',
 ]
 
 import logging
 import sys
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, MINYEAR, MAXYEAR, date, time, tzinfo
+from enum import IntEnum
 from functools import wraps
 from threading import Lock
+from typing import Generator, Optional
 
 logger = logging.getLogger('zeraora.datetime')
 
@@ -202,3 +211,397 @@ class BearTimer:
         """
         _, _, curr, _, _ = self.lap(msg)
         return curr
+
+
+def is_leap(year: int) -> bool:
+    """
+    判断一个年份是否为闰年。
+    """
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def get_last_monthday(year: int, month: int) -> int:
+    """
+    计算某年某月的最后一天是哪一天。
+    """
+    if month == 2 and is_leap(year):
+        return 29
+    return Datetime.DAYS_IN_MONTH[month]
+
+
+class TimeFrame(IntEnum):
+    YEAR = 0
+    MONTH = 1
+    DAY = 2
+    HOUR = 3
+    MINUTE = 4
+    SECOND = 5
+    MICROSECOND = 6
+
+
+class Datetime(datetime):
+    """
+    增强型日期时间对象。
+    """
+
+    DAYS_IN_MONTH = (-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+    # ---- 构造器 ----
+
+    @classmethod
+    def fromcalendar(cls, year: int, week: int, day: int, sunday_first=False) -> Datetime:
+        """
+        将某一年某一周的星期几转换为一个具体的日期。
+
+        - ``sunday_first=False`` 时一年内第一个周一之前的日子都属于第 ``0`` 周。
+        - ``sunday_first=True`` 时一年内第一个周日之前的日子都属于第 ``0`` 周。
+
+        :param year: 具体年份。比如 2012、2023 等。
+        :param week: 第几周。从 ``0`` 开始。
+        :param day: 星期几。``0`` 表示周日、``1`` 表示周一，以此类推。
+        :param sunday_first: 是否将周日作为一周的开始。
+        :return: 一个具体的日期。
+        """
+        return cls.strptime(
+            f'{year:04d}-{week:02d}-{day:1d}',
+            '%Y-%U-%w' if sunday_first else '%Y-%W-%w',
+        )
+
+    @classmethod
+    def fromdatetime(cls, _datetime: datetime) -> Datetime:
+        """
+        用 :class:`datetime` 构造对象。
+        """
+        return cls(
+            _datetime.year, _datetime.month, _datetime.day,
+            _datetime.hour, _datetime.minute, _datetime.second,
+            _datetime.microsecond, _datetime.tzinfo, fold=_datetime.fold,
+        )
+
+    of = fromdatetime
+
+    # ---- 转换器 ----
+
+    def replace(
+            self,
+            year: Optional[int] = None,
+            month: Optional[int] = None,
+            day: Optional[int] = None,
+            hour: Optional[int] = None,
+            minute: Optional[int] = None,
+            second: Optional[int] = None,
+            microsecond: Optional[int] = None,
+            _tzinfo: Optional[tzinfo] = ...,
+            *,
+            fold: Optional[int] = None,
+            standard: bool = False,
+    ) -> Datetime | datetime:
+        """
+        修改部分字段并返回一个新的日期时间。
+
+        如果 ``standard=True`` 则固定返回 :class:`datetime` 对象。
+        """
+        return (datetime if standard else type(self))(
+            year or self.year,
+            month or self.month,
+            day or self.day,
+            hour or self.hour,
+            minute or self.minute,
+            second or self.second,
+            microsecond or self.microsecond,
+            self.tzinfo if _tzinfo is ... else _tzinfo,
+            fold=fold or self.fold,
+        )
+
+    @classmethod
+    def combine(
+            cls,
+            a: date | datetime,
+            b: time | datetime | timedelta,
+            _tzinfo: Optional[tzinfo] = ...
+    ) -> Datetime:
+        """
+        将 a 的日期部分和 b 的时间部分组合为一个新的日期时间。
+
+        :param a: 支持 :class:`data` 或 :class:`datetime` 及其子类的对象。
+        :param b: 支持 :class:`time`、:class:`datetime` 或 :class:`timedelta` 及其子类的对象。
+        :param _tzinfo: 时区。默认使用 b 的时区设置，如不需要可设置为 ``None`` 。
+        :return: 组合得到的日期时间。
+        """
+        if _tzinfo is ...:
+            _tzinfo = b.tzinfo
+        if not isinstance(a, (date, datetime)):
+            raise TypeError("argument 'a' must be a date or datetime instance")
+        if not isinstance(b, (time, datetime, timedelta)):
+            raise TypeError("argument 'b' must be a time, datetime, or timedelta instance")
+        if isinstance(b, timedelta):
+            b = Timedelta.of(b).base()
+
+        return cls(
+            a.year, a.month, a.day,
+            b.hour, b.minute, b.second, b.microsecond,
+            _tzinfo, fold=b.fold,
+        )
+
+    def empty(
+            self,
+            keeping_level: TimeFrame = TimeFrame.DAY,
+            year: Optional[int] = None,
+            month: Optional[int] = None,
+            day: Optional[int] = None,
+            hour: Optional[int] = None,
+            minute: Optional[int] = None,
+            second: Optional[int] = None,
+            microsecond: Optional[int] = None,
+    ) -> Datetime:
+        """
+        将某个层级以下（不含）的部分归零。
+
+        >>> now = Datetime.now()
+        >>> for frame in TimeFrame:
+        >>>     print(
+        >>>         now.empty(frame), frame, sep='\\t'
+        >>>     )
+        2024-01-01 00:00:00         TimeFrame.YEAR
+        2024-09-01 00:00:00         TimeFrame.MONTH
+        2024-09-17 00:00:00         TimeFrame.DAY
+        2024-09-17 11:00:00         TimeFrame.HOUR
+        2024-09-17 11:22:00         TimeFrame.MINUTE
+        2024-09-17 11:22:33         TimeFrame.SECOND
+        2024-09-17 11:22:33.245678  TimeFrame.MICROSECOND
+
+        可以将时间的某个部分固定为指定值，比如将小时固定为凌晨四点：
+
+        >>> for frame in TimeFrame:
+        >>>     print(
+        >>>         now.empty(frame, hour=4), frame, sep='\\t'
+        >>>     )
+        2024-01-01 04:00:00         TimeFrame.YEAR
+        2024-09-01 04:00:00         TimeFrame.MONTH
+        2024-09-17 04:00:00         TimeFrame.DAY
+        2024-09-17 04:00:00         TimeFrame.HOUR
+        2024-09-17 04:22:00         TimeFrame.MINUTE
+        2024-09-17 04:22:33         TimeFrame.SECOND
+        2024-09-17 04:22:33.245678  TimeFrame.MICROSECOND
+        """
+        index = keeping_level + 1
+        origin = (
+            year or self.year,
+            month or self.month,
+            day or self.day,
+            hour or self.hour,
+            minute or self.minute,
+            second or self.second,
+            microsecond or self.microsecond,
+        )
+        specified = (
+            year or MINYEAR,
+            month or 1,
+            day or 1,
+            hour or 0,
+            minute or 0,
+            second or 0,
+            microsecond or 0,
+        )
+        return type(self)(
+            *(origin[:index] + specified[index:]),
+            tzinfo=self.tzinfo, fold=self.fold,
+        )
+
+    def fill(
+            self,
+            keeping_level: TimeFrame = TimeFrame.DAY,
+            year: Optional[int] = None,
+            month: Optional[int] = None,
+            day: Optional[int] = None,
+            hour: Optional[int] = None,
+            minute: Optional[int] = None,
+            second: Optional[int] = None,
+            microsecond: Optional[int] = None,
+    ) -> Datetime:
+        """
+        将某个层级以下（不含）的部分填满。
+
+        >>> now = Datetime.now()
+        >>> for frame in TimeFrame:
+        >>>     print(
+        >>>         now.fill(frame), frame, sep='\\t'
+        >>>     )
+        2024-12-31 23:59:59.999999    TimeFrame.YEAR
+        2024-09-30 23:59:59.999999    TimeFrame.MONTH
+        2024-09-09 23:59:59.999999    TimeFrame.DAY
+        2024-09-09 11:59:59.999999    TimeFrame.HOUR
+        2024-09-09 11:22:59.999999    TimeFrame.MINUTE
+        2024-09-09 11:22:33.999999    TimeFrame.SECOND
+        2024-09-09 11:22:33.245678    TimeFrame.MICROSECOND
+
+        可以将时间的某个部分固定为给定值，比如将小时固定为凌晨四点：
+
+        >>> for frame in TimeFrame:
+        >>>     print(
+        >>>         now.fill(frame), frame, sep='\\t'
+        >>>     )
+        2024-12-31 04:59:59.999999    TimeFrame.YEAR
+        2024-09-30 04:59:59.999999    TimeFrame.MONTH
+        2024-09-09 04:59:59.999999    TimeFrame.DAY
+        2024-09-09 04:59:59.999999    TimeFrame.HOUR
+        2024-09-09 04:22:59.999999    TimeFrame.MINUTE
+        2024-09-09 04:22:33.999999    TimeFrame.SECOND
+        2024-09-09 04:22:33.245678    TimeFrame.MICROSECOND
+        """
+        index = keeping_level + 1
+        origin = [
+            year or self.year,
+            month or self.month,
+            day or self.day,
+            hour or self.hour,
+            minute or self.minute,
+            second or self.second,
+            microsecond or self.microsecond,
+        ]
+        specified = [
+            year or MAXYEAR,
+            month or 12,
+            day or 31,
+            hour or 23,
+            minute or 59,
+            second or 59,
+            microsecond or 999999,
+        ]
+        result = origin[:index] + specified[index:]
+        result[2] = min(result[2], get_last_monthday(result[0], result[1]))
+        return type(self)(*result, tzinfo=self.tzinfo, fold=self.fold)
+
+    def calendar(self, sunday_first=False) -> tuple[int, int, int]:
+        """
+        计算当前日期是哪一年哪一周的周几。
+
+        - ``sunday_first=False`` 时一年内第一个周一之前的日子都属于第 ``0`` 周。
+        - ``sunday_first=True`` 时一年内第一个周日之前的日子都属于第 ``0`` 周。
+
+        :param sunday_first: 是否将周日作为一周的开始。
+        :return: 一个三元组，分别表示哪一、哪一周、周几。
+        """
+        return tuple(map(int, self.strftime('%Y-%U-%w' if sunday_first else '%Y-%W-%w').split('-')))
+
+    # ---- 判断 ----
+
+    def is_leap(self) -> bool:
+        """
+        当年是否为闰年。
+        """
+        return self.year % 4 == 0 and (self.year % 100 != 0 or self.year % 400 == 0)
+
+    # ---- 属性 ----
+
+    @property
+    def last_monthday(self) -> int:
+        """
+        当月的最后一天是哪一天。
+        """
+        if self.month == 2 and self.is_leap():
+            return 29
+        return self.DAYS_IN_MONTH[self.month]
+
+
+class Timedelta(timedelta):
+    """
+    增强型时间差对象。
+    """
+
+    # ---- 构造器 ----
+
+    @classmethod
+    def fromtime(cls, _time: time) -> Timedelta:
+        """
+        用 :class:`time` 构造时间差对象。
+
+        换句话说就是 fromtime(_time) = _time - 00:00:00
+        """
+        return cls(
+            seconds=_time.hour * 3600 + _time.minute * 60 + _time.second,
+            microseconds=_time.microsecond,
+        )
+
+    @classmethod
+    def fromtimedelta(cls, delta: timedelta) -> Timedelta:
+        """
+        用 :class:`timedelta` 构造时间差对象。
+        """
+        return cls(days=delta.days, seconds=delta.seconds, microseconds=delta.microseconds)
+
+    of = fromtimedelta
+
+    # ---- 转换 ----
+
+    def replace(
+            self,
+            days: Optional[int] = None,
+            seconds: Optional[int] = None,
+            microseconds: Optional[int] = None,
+            standard: bool = False,
+    ) -> Timedelta | timedelta:
+        """
+        修改部分字段并返回一个新的时间差对象。
+
+        如果 ``standard=True`` 则固定返回 :class:`timedelta` 对象。
+        """
+        return (timedelta if standard else type(self))(
+            days or self.days,
+            seconds or self.seconds,
+            microseconds or self.microseconds,
+        )
+
+    def base(self, _time: Optional[time] = None) -> time:
+        """
+        基于某个时间叠加得到另一个时间。
+
+        **该方法可能会在将在被废弃**！
+        """
+        if _time is None:
+            s = self.seconds
+            ms = self.microseconds
+        else:
+            ms = _time.microsecond + self.microseconds
+            s = ms // 10 ** 6 + self.seconds
+            ms = ms % 10 ** 6
+        return time(
+            hour=s // 3600 % 24,
+            minute=s % 3600 // 60,
+            second=s % 60,
+            microsecond=ms,
+        )
+
+
+def daterange(start: date, stop: date, step: int = 1, closed=False) -> Generator[date, None, None]:
+    """
+    在一个日期范围内，按指定的步长迭代生成 :class:`date` 对象。
+
+    :param start: 开始日期。
+    :param stop: 结束日期。
+    :param step: 步长。提供负数时，请确保开始日期晚于（即大于）结束日期。如果为 ``0`` 会引发 :class:`datetime` 。
+    :param closed: 结束日期是否可以到达。
+    :return: 生成 :class:`date` 对象的生成器。
+    """
+    if closed:
+        ordinals = range(start.toordinal(), stop.toordinal() + step, step)
+    else:
+        ordinals = range(start.toordinal(), stop.toordinal(), step)
+
+    return (date.fromordinal(o) for o in ordinals)
+
+
+def weekrange(year: int, week: int, sunday_first=False) -> Generator[date, None, None]:
+    """
+    枚举生成某一周的所有日期。
+
+    :param year: 具体年份。比如 2012、2023 等。
+    :param week: 一年中的第几周。从 ``0`` 开始。
+    :param sunday_first: 是否以周日为一周的开始。
+    :return: 生成 :class:`date` 对象的生成器。
+    """
+    start = datetime.strptime(
+        f'{year:04d}-{week:02d}-{0 if sunday_first else 1}',
+        '%Y-%U-%w' if sunday_first else '%Y-%W-%w',
+    ).date()
+    return (start + timedelta(days=i) for i in range(7))
