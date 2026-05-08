8175cc79e0678756acfe756e851f1110081fd290
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-05-08 17:14:32 +0800

    refactor(uuid): Python 3.14+ 直接用 uuid 库的 uuid7、uuid8 代替

--- zeraora/uuid.py
+++ zeraora/uuid.py
@@ -4,51 +4,56 @@ __all__ = [
     'uuid8i',
 ]
 
+import sys
 from random import getrandbits
 from time import time_ns
 from uuid import UUID
 
+if sys.version_info >= (3, 14):
+    from uuid import uuid7
+else:
+    def uuid7() -> UUID:
+        """
+        根据 RFC 9562 定义的第七版 UUID 生成一个带有毫秒级时间戳和一个随机数的 UUID 对象。
 
-def uuid7() -> UUID:
-    """
-    根据 RFC 9562 定义的第七版 UUID 生成一个带有毫秒级时间戳和一个随机数的 UUID 对象。
+        注意：此函数使用了 :class:`random.Random`，因此不应将其用于安全目的。
+        """
+        return UUID(int=(
+                0
+                | time_ns() // 1000_000 << 80
+                | 0x7 << 76  # 表明是 RFC 9562 定义的第七版 UUID
+                | getrandbits(12) << 64
+                | 0b10 << 62  # 表明是 RFC 9562 定义的 UUID
+                | getrandbits(62)
+        ))
 
-    注意：此函数使用了 :class:`random.Random`，因此不应将其用于安全目的。
-    """
-    return UUID(int=(
-            0
-            | time_ns() // 1000_000 << 80
-            | 0x7 << 76  # 表明是 RFC 9562 定义的第七版 UUID
-            | getrandbits(12) << 64
-            | 0b10 << 62  # 表明是 RFC 9562 定义的 UUID
-            | getrandbits(62)
-    ))
+if sys.version_info >= (3, 14):
+    from uuid import uuid8
+else:
+    def uuid8(
+            a: int = None,
+            b: int = None,
+            c: int = None,
+    ) -> UUID:
+        """
+        根据 RFC 9562 定义的第八版 UUID 生成一个自定义结构的 UUID。
 
+        三个参数预期为三个 48、12、62
+        比特的非负整数；如果超出长度，则仅保留最低有效位；若为负数，则将会取绝对值；如果没有提供，则替换成适当大小的伪随机数。
 
-def uuid8(
-        a: int = None,
-        b: int = None,
-        c: int = None,
-) -> UUID:
-    """
-    根据 RFC 9562 定义的第八版 UUID 生成一个自定义结构的 UUID。
-
-    三个参数预期为三个 48、12、62
-    比特的非负整数；如果超出长度，则仅保留最低有效位；若为负数，则将会取绝对值；如果没有提供，则替换成适当大小的伪随机数。
-
-    注意：伪随机数使用了 :class:`random.Random` 生成，因此若是不提供参数，那么不应将本函数用于安全目的。
-    """
-    a = abs(getrandbits(48) if a is None else a) & 0xFFFF_FFFF_FFFF
-    b = abs(getrandbits(12) if b is None else b) & 0x0FFF
-    c = abs(getrandbits(62) if c is None else c) & 0x3FFF_FFFF_FFFF_FFFF
-    return UUID(int=(
-            0
-            | a << 80
-            | 0x8 << 76  # 表明是 RFC 9562 定义的第八版 UUID
-            | b << 64
-            | 0b10 << 62  # 表明是 RFC 9562 定义的 UUID
-            | c
-    ))
+        注意：伪随机数使用了 :class:`random.Random` 生成，因此若是不提供参数，那么不应将本函数用于安全目的。
+        """
+        a = abs(getrandbits(48) if a is None else a) & 0xFFFF_FFFF_FFFF
+        b = abs(getrandbits(12) if b is None else b) & 0x0FFF
+        c = abs(getrandbits(62) if c is None else c) & 0x3FFF_FFFF_FFFF_FFFF
+        return UUID(int=(
+                0
+                | a << 80
+                | 0x8 << 76  # 表明是 RFC 9562 定义的第八版 UUID
+                | b << 64
+                | 0b10 << 62  # 表明是 RFC 9562 定义的 UUID
+                | c
+        ))
 
 
 def uuid8i(integer: int) -> UUID:
