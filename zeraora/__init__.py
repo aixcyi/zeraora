645cfeafe8275deeaa03d5180419689d69396d74
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-05-21 16:22:48 +0800

    refactor: 版本号增设 candidate 阶段

--- zeraora/__init__.py
+++ zeraora/__init__.py
@@ -1,20 +1,23 @@
-from typing import Literal as _Literal, NamedTuple as _NamedTuple
+from typing import (
+    Literal as _Literal,
+    NamedTuple as _NamedTuple,
+)
 
 
 class _VersionInfo(_NamedTuple):
     major: int
     minor: int
     micro: int
-    level: _Literal['alpha', 'beta', 'final']  # Zeraora 不需要 candidate 级别
+    level: _Literal['alpha', 'beta', 'candidate', 'final']
     serial: int
 
     # https://packaging.python.org/en/latest/specifications/version-specifiers/
     def __str__(self):
-        level = {'alpha': 'a', 'beta': 'b', 'final': ''}[self.level]
+        level = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}[self.level]
         return f'{self.major}.{self.minor}.{self.micro}{level}.{self.serial}'
 
     def __int__(self):
-        level = {'alpha': 0xA, 'beta': 0xB, 'final': 0xF}[self.level]
+        level = {'alpha': 0xA, 'beta': 0xB, 'candidate': 0xC, 'final': 0xF}[self.level]
         return self.major << 24 | self.minor << 16 | self.micro << 8 | level << 4 | self.serial
 
     # noinspection SpellCheckingInspection
@@ -27,6 +30,6 @@ class _VersionInfo(_NamedTuple):
         return self.level == 'final'
 
 
-VERSION = _VersionInfo(0, 4, 0, 'final', 6)
+VERSION = _VersionInfo(0, 4, 0, 'candidate', 1)
 
 __version__ = str(VERSION)
